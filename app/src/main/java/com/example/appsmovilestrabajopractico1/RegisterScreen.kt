432f45c56c23fc3c199d9f736862b278248aeb96
Tawanchai+ <gastonferrarisdavies@gmail.com>
2025-04-15 17:44:04 -0300

    SimpleNavbar

--- app/src/main/java/com/example/appsmovilestrabajopractico1/RegisterScreen.kt
+++ app/src/main/java/com/example/appsmovilestrabajopractico1/RegisterScreen.kt
@@ -7,6 +7,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
 
 @Composable
 fun RegisterScreen(navController: NavHostController) {
@@ -16,67 +20,83 @@ fun RegisterScreen(navController: NavHostController) {
     var confirmPassword by remember { mutableStateOf("") }
     var errorMessage by remember { mutableStateOf("") }
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        verticalArrangement = Arrangement.Center
-    ) {
-        TextField(
-            value = name,
-            onValueChange = { name = it },
-            label = { Text("Nombre") },
-            modifier = Modifier.fillMaxWidth()
-        )
 
-        Spacer(modifier = Modifier.height(8.dp))
-
-        TextField(
-            value = email,
-            onValueChange = { email = it },
-            label = { Text("Email") },
-            modifier = Modifier.fillMaxWidth()
-        )
-
-        Spacer(modifier = Modifier.height(8.dp))
-
-        TextField(
-            value = password,
-            onValueChange = { password = it },
-            label = { Text("Contraseña") },
+        Row(
             modifier = Modifier.fillMaxWidth(),
-            visualTransformation = PasswordVisualTransformation()
-        )
+            verticalAlignment = Alignment.CenterVertically) {
 
-        Spacer(modifier = Modifier.height(8.dp))
+            Text ( text = "Registrate aquí!", color = Color.Black,
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(24.dp),
+            )
 
-        TextField(
-            value = confirmPassword,
-            onValueChange = { confirmPassword = it },
-            label = { Text("Repetir Contraseña") },
-            modifier = Modifier.fillMaxWidth(),
-            visualTransformation = PasswordVisualTransformation()
-        )
-
-        Spacer(modifier = Modifier.height(8.dp))
-
-        if (errorMessage.isNotEmpty()) {
-            Text(text = errorMessage, color = MaterialTheme.colorScheme.error)
-            Spacer(modifier = Modifier.height(8.dp))
+            IconButton(onClick = { navController.navigate("welcome") }) {
+                Icon(Icons.Filled.ArrowBack, contentDescription = "Volver", tint = Color.Black)
+            }
         }
 
-        Button(onClick = {
-            errorMessage = when {
-                name.isBlank() || email.isBlank() -> "Nombre y Email no pueden estar vacíos"
-                password.length < 6 -> "La contraseña debe tener al menos 6 caracteres"
-                password != confirmPassword -> "Las contraseñas no coinciden"
-                else -> {
-                    navController.popBackStack() // Volver al login
-                    ""
-                }
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center
+        ) {
+            TextField(
+                value = name,
+                onValueChange = { name = it },
+                label = { Text("Nombre") },
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            TextField(
+                value = email,
+                onValueChange = { email = it },
+                label = { Text("Email") },
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            TextField(
+                value = password,
+                onValueChange = { password = it },
+                label = { Text("Contraseña") },
+                modifier = Modifier.fillMaxWidth(),
+                visualTransformation = PasswordVisualTransformation()
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            TextField(
+                value = confirmPassword,
+                onValueChange = { confirmPassword = it },
+                label = { Text("Repetir Contraseña") },
+                modifier = Modifier.fillMaxWidth(),
+                visualTransformation = PasswordVisualTransformation()
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            if (errorMessage.isNotEmpty()) {
+                Text(text = errorMessage, color = MaterialTheme.colorScheme.error)
+                Spacer(modifier = Modifier.height(8.dp))
             }
-        }, modifier = Modifier.fillMaxWidth()) {
-            Text("Registrarse")
+
+            Button(onClick = {
+                errorMessage = when {
+                    name.isBlank() || email.isBlank() -> "Nombre y Email no pueden estar vacíos"
+                    password.length < 6 -> "La contraseña debe tener al menos 6 caracteres"
+                    password != confirmPassword -> "Las contraseñas no coinciden"
+                    else -> {
+                        navController.popBackStack() // Volver al login
+                        ""
+                    }
+                }
+            }, modifier = Modifier.fillMaxWidth()) {
+                Text("Registrarse")
+            }
         }
     }
-}
